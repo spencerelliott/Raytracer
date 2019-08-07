55df1a284b4cf0ac82101c4d2a562aad84cc0d8e
Spencer Elliott <charles@gamehive.com>
2019-08-06 20:17:44 -0400

    Added more operators

--- Raytracer/Vec3.h
+++ Raytracer/Vec3.h
@@ -35,13 +35,86 @@ public:
 	inline float operator[](int i) const { return e[i]; }
 	inline float& operator[](int i) { return e[i]; }
 
-	inline vec3& operator+=(const vec3& v2);
-	inline vec3& operator-=(const vec3& v2);
-	inline vec3& operator*=(const vec3& v2);
-	inline vec3& operator/=(const vec3& v2);
+	inline vec3& operator+=(const vec3& v);
+	inline vec3& operator-=(const vec3& v);
+	inline vec3& operator*=(const vec3& v);
+	inline vec3& operator/=(const vec3& v);
 
 	inline vec3& operator*=(const float t);
 	inline vec3& operator/=(const float t);
 
+private:
 	float e[3] = { 0.0f };
 };
+
+inline std::istream& operator>>(std::istream& is, vec3& v) {
+	is >> v[0] >> v[1] >> v[2];
+	return is;
+}
+
+inline std::ostream& operator<<(std::ostream& os, vec3& v)
+{
+	os << v[0] << " " << v[1] << v[2];
+	return os;
+}
+
+inline void vec3::make_unit_vector() {
+	float k = 1.0 / length();
+	e[0] *= k;
+	e[1] *= k;
+	e[2] *= k;
+}
+
+inline vec3 operator+(const vec3& v1, const vec3& v2) {
+	return vec3(v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]);
+}
+
+inline vec3 operator-(const vec3& v1, const vec3& v2) {
+	return vec3(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]);
+}
+
+inline vec3 operator*(const vec3& v1, const vec3& v2) {
+	return vec3(v1[0] * v2[0], v1[1] * v2[1], v1[2] * v2[2]);
+}
+
+inline vec3 operator/(const vec3& v1, const vec3& v2) {
+	return vec3(v1[0] / v2[0], v1[1] / v2[1], v1[2] / v2[2]);
+}
+
+inline vec3 operator*(float t, const vec3& v) {
+	return vec3(t * v[0], t * v[1], t * v[2]);
+}
+
+inline vec3 operator*(const vec3& v, float t) {
+	return vec3(t * v[0], t * v[1], t * v[2]);
+}
+
+inline vec3 operator/(const vec3& v, float t) {
+	return vec3(v[0] / t, v[1] / t, v[2] / t);
+}
+
+inline float dot(const vec3& v1, const vec3& v2) {
+	return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+}
+
+inline vec3 cross(const vec3& v1, const vec3& v2) {
+	return vec3(v1[1] * v2[2] - v1[2] * v2[1],
+		-(v1[0] * v2[2] - v1[2] * v2[0]),
+		v1[0] * v2[1] - v1[1] * v2[0]);
+}
+
+inline vec3& vec3::operator+=(const vec3& v) {
+	e[0] += v[0];
+	e[1] += v[1];
+	e[2] += v[2];
+
+	return *this;
+}
+
+inline vec3& vec3::operator-=(const vec3& v) {
+	e[0] -= v[0];
+	e[1] -= v[1];
+	e[2] -= v[2];
+
+	return *this;
+}
