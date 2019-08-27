3c2eecd0560378f627264494bc3290a9037b1edf
Spencer Elliott <charles@gamehive.com>
2019-08-27 12:50:39 -0400

    Added abstract class to determine hits

--- Raytracer/main.cpp
+++ Raytracer/main.cpp
@@ -1,37 +1,26 @@
 #include <iostream>
 #include <fstream>
-#include <vec3.h>
-#include <ray.h>
+#include <float.h>
+
+#include "vec3.h"
+#include "ray.h"
+#include "sphere.h"
+#include "hitable_list.h"
 
 #define FLOATCOLOR(c) int(255.99 * c)
 #define DRAWPIXEL(r, g, b) std::cout << r << " " << g << " " << b << "\n"
 
-float hit_sphere(const vec3& center, float radius, const ray& r) {
-	vec3 oc = r.origin() - center;
-	float a = dot(r.direction(), r.direction());
-	float b = 2.0 * dot(oc, r.direction());
-	float c = dot(oc, oc) - radius * radius;
-
-	float discriminant = b * b - 4 * a * c;
-
-	if (discriminant < 0) {
-		return -1.0;
+vec3 color(const ray& r, hitable *world) {
+	hit_record rec;
+	if (world->hit(r, 0.0, FLT_MAX, rec)) {
+		return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
 	}
 	else {
-		return (-b - sqrt(discriminant)) / (2.0 * a);
-	}
-}
+		vec3 unit_direction = make_unit_vector(r.direction());
+		float t = 0.5 * (unit_direction.y() + 1.0f);
 
-vec3 color(const ray& r) {
-	float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
-	if (t > 0.0) {
-		vec3 norm = make_unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
-		return 0.5 * vec3(norm.x() + 1, norm.y() + 1, norm.z() + 1);
+		return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 	}
-	vec3 unit_direction = make_unit_vector(r.direction());
-	t = 0.5 * (unit_direction.y() + 1.0f);
-
-	return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -43,6 +32,13 @@ int main() {
 	vec3 vertical(0.0, 2.0, 0.0);
 	vec3 origin(0.0, 0.0, 0.0);
 
+	hitable* list[2];
+
+	list[0] = new sphere(vec3(0, 0, -1), 0.5);
+	list[1] = new sphere(vec3(0, -100.5, -1), 100);
+
+	hitable* world = new hitable_list(list, 2);
+
 	int w = 200;
 	int h = 100;
 	std::cout << "P3\n" << w << " " << h << "\n255\n";
@@ -50,8 +46,9 @@ int main() {
 		for (int i = 0; i < w; i++) {
 			float u = float(i) / float(w);
 			float v = float(j) / float(h);
+			ray r = ray(origin, lower_left + u * horizontal + v * vertical);
 
-			vec3 col = color(ray(origin, lower_left + u*horizontal + v*vertical));
+			vec3 col = color(r, world);
 
 			int ir = FLOATCOLOR(col.r());
 			int ig = FLOATCOLOR(col.g());
