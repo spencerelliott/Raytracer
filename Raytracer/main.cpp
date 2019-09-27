3cab077bd7f552962ece5452b368132ae15bc361
Spencer Elliott <s.elliott123@gmail.com>
2019-09-27 13:36:23 -0400

    Started work on real-time path tracing

--- Raytracer/main.cpp
+++ Raytracer/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <float.h>
 #include <Windows.h>
+#include <time.h>
 
 #include "vec3.h"
 #include "ray.h"
@@ -11,8 +12,11 @@
 #include "random.h"
 #include "material.h"
 
+#include <SDL.h>
+
 #define FLOATCOLOR(c) int(255.99 * c)
-#define DRAWPIXEL(r, g, b) std::cout << r << " " << g << " " << b << "\n"
+#define DRAWPIXEL(r, g, b) out << r << " " << g << " " << b << "\n"
+#define NUM_THREADS 4
 
 struct scene_info {
 	int seed;
@@ -47,6 +51,11 @@ vec3 color(const ray& r, hitable *world, int depth) {
 	}
 }
 
+HANDLE thread_mutex;
+
+int thread_run_count = 0;
+bool continue_threads = false;
+
 DWORD WINAPI raytrace(LPVOID param) {
 	scene_info* scene = (scene_info*)param;
 
@@ -57,39 +66,50 @@ DWORD WINAPI raytrace(LPVOID param) {
 	hitable* world = scene->world;
 	camera cam = *scene->cam;
 
-	int pixel = 0;
+	while (1) {
+		int pixel = 0;
 
-	for (int j = h - 1; j >= 0; j--) {
-		for (int i = 0; i < w; i++) {
-			rng_state = scene->seed;
+		for (int j = h - 1; j >= 0; j--) {
+			for (int i = 0; i < w; i++) {
+				rng_state = scene->seed;
 
-			vec3 col(0, 0, 0);
+				vec3 col(0, 0, 0);
 
-			for (int s = 0; s < samples; s++) {
-				float u = float(i + random_0_to_1()) / float(w);
-				float v = float(j + random_0_to_1()) / float(h);
-				ray r = cam.get_ray(u, v);
+				for (int s = 0; s < samples; s++) {
+					float u = float(i + random_0_to_1()) / float(w);
+					float v = float(j + random_0_to_1()) / float(h);
+					ray r = cam.get_ray(u, v);
 
-				col += color(r, world, 0);
+					col += color(r, world, 0);
+				}
+
+				col /= float(samples);
+				col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+
+				scene->pixels[pixel++] = col.r();
+				scene->pixels[pixel++] = col.g();
+				scene->pixels[pixel++] = col.b();
+
+				scene->seed = rng_state;
 			}
+		}
 
-			col /= float(samples);
-			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+		DWORD wait_result = WaitForSingleObject(thread_mutex, INFINITE);
 
-			scene->pixels[pixel++] = col.r();
-			scene->pixels[pixel++] = col.g();
-			scene->pixels[pixel++] = col.b();
+		thread_run_count--;
 
-			scene->seed = rng_state;
+		ReleaseMutex(thread_mutex);
+
+		while (thread_run_count < NUM_THREADS) {
+			Sleep(10);
 		}
 	}
 
 	return 0;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
 	std::ofstream out("out.ppm");
-	std::cout.rdbuf(out.rdbuf());
 
 	vec3 lower_left(-2.0, -1.0, -1.0);
 	vec3 horizontal(4.0, 0.0, 0.0);
@@ -105,21 +125,28 @@ int main() {
 
 	hitable* world = new hitable_list(list, 4);
 
-	int num_threads = 4;
+	int num_threads = NUM_THREADS;
 
-	int m = 4;
-	int ms = 4;
+	int m = 2;
+	int ms = 1;
 
 	int w = m * 320;
 	int h = m * 180;
-	int samples = ms * 25;
+	int samples = ms * 5;
 
 	int samples_per_thread = samples / num_threads;
 
 	camera cam(vec3(0, 0.4, 0.5), vec3(0, 0, -1), vec3(0, 1, 0), 90, float(w) / float(h));
 
+	thread_mutex = CreateMutex(
+		NULL,              // default security attributes
+		FALSE,             // initially not owned
+		NULL);
+
 	HANDLE* threads = new HANDLE[num_threads];
-	scene_info scenes[4];
+	scene_info scenes[NUM_THREADS];
+
+	long start_time = time(0);
 
 	for (int i = 0; i < num_threads; i++) {	
 		scenes[i].seed = wang_hash(i);
@@ -133,31 +160,56 @@ int main() {
 		threads[i] = CreateThread(NULL, 0, raytrace, &scenes[i], 0, 0);
 	}
 
-	WaitForMultipleObjects(num_threads, threads, true, INFINITE);
+	//WaitForMultipleObjects(num_threads, threads, true, INFINITE);
 	
-	std::cout << "P3\n" << w << " " << h << "\n255\n";
+	out << "P3\n" << w << " " << h << "\n255\n";
 
-	for (int i = 0; i < w * h * 3; i += 3) {
-		float avg_r = 0.0;
-		float avg_g = 0.0;
-		float avg_b = 0.0;
+	SDL_Event event;
+	SDL_Renderer* renderer;
+	SDL_Window* window;
+	int i;
 
-		for (int k = 0; k < num_threads; k++) {
-			float r = (float)scenes[k].pixels[i];
-			float g = (float)scenes[k].pixels[i + 1];
-			float b = (float)scenes[k].pixels[i + 2];
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_CreateWindowAndRenderer(w, h, 0, &window, &renderer);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+	SDL_RenderClear(renderer);
 
-			avg_r += r;
-			avg_g += g;
-			avg_b += b;
+	while (1) {
+		while (thread_run_count > 0);
+
+		for (int i = 0; i < w * h * 3; i += 3) {
+			float avg_r = 0.0;
+			float avg_g = 0.0;
+			float avg_b = 0.0;
+
+			for (int k = 0; k < num_threads; k++) {
+				float r = (float)scenes[k].pixels[i];
+				float g = (float)scenes[k].pixels[i + 1];
+				float b = (float)scenes[k].pixels[i + 2];
+
+				avg_r += r;
+				avg_g += g;
+				avg_b += b;
+			}
+
+			int ir = FLOATCOLOR(avg_r / num_threads);
+			int ig = FLOATCOLOR(avg_g / num_threads);
+			int ib = FLOATCOLOR(avg_b / num_threads);
+
+			//DRAWPIXEL(ir, ig, ib);
+			SDL_SetRenderDrawColor(renderer, ir, ig, ib, 255);
+			SDL_RenderDrawPoint(renderer, (i / 3) % w, (i / 3) / h);
 		}
 
-		int ir = FLOATCOLOR(avg_r / num_threads);
-		int ig = FLOATCOLOR(avg_g / num_threads);
-		int ib = FLOATCOLOR(avg_b / num_threads);
+		SDL_RenderPresent(renderer);
 
-		DRAWPIXEL(ir, ig, ib);
+		if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+			break;
+
+		thread_run_count = num_threads;
 	}
+
+	long end_time = time(0);
 
 	/*for (int j = h - 1; j >= 0; j--) {
 		for (int i = 0; i < w; i++) {
@@ -181,4 +233,15 @@ int main() {
 			DRAWPIXEL(ir, ig, ib);
 		}
 	}*/
+
+	std::cout << "Took " << (end_time - start_time) << "ms to render";
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	out.flush();
+	out.close();
+
+	return 0;
 }
